6cf10f8130247d0522134368af8a3eeb53098f37
shivam-kotwalia <shivamkotwalia@gmail.com>
2019-09-07 14:06:57 +0530

    Added tests

--- jup2py/jup2pylib.py
+++ jup2py/jup2pylib.py
@@ -1,27 +1,30 @@
-import os 
+import os
 import nbconvert
 
+
 def jup2py(jup_filename, py_filename=None, keep_org=False):
-    print("Please make sure you have saved your jupyter notebook before converting")
+    print(
+        "Please make sure you have saved your jupyter notebook before converting"
+    )
     if jup_filename is None:
         print("Please supply jupter filename with .ipynb extension")
     if py_filename is None:
         py_org_filename = os.path.splitext(jup_filename)[0] + "_original.py"
         py_filename = os.path.splitext(jup_filename)[0] + ".py"
 
-    #Converting jupyter to py through nbconvertor
+    # Converting jupyter to py through nbconvertor
     py_code = nbconvert.export(nbconvert.PythonExporter(), jup_filename)
     with open(py_org_filename, "w") as myfile:
         myfile.write(py_code[0])
-    
+
     with open(py_org_filename, "r") as myfile:
         mycode = myfile.readlines()
     mycode2 = []
     for index, line in enumerate(mycode):
-        #Removing which are either "\n" or denotes the "Input" Tag for jupyter notebook
+        # Removing which are either "\n" or denotes the "Input" Tag for jupyter notebook
         if line == "\n" or (line.startswith("# In[")):
             pass
-        # Commenting the magic functions or the command line 
+        # Commenting the magic functions or the command line
         elif "get_ipython()" in line:
             line = "#" + line
             mycode2.append(line)
